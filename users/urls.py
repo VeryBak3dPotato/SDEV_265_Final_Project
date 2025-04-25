1986edfa105e6cc48402cec78792da8da86d2257
Connor Bailey <cebailey92@gmail.com>
2025-04-25 03:37:05 +0000

    rearranged urls

--- users/urls.py
+++ users/urls.py
@@ -4,11 +4,11 @@ from .views import *
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('register/', RegisterUser.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('hello/', HelloView.as_view(), name='hello'),
     path('users/<int:pk>/current/', WeatherCurrentView.as_view(), name='weather-current'),
     path('users/<int:pk>/minutely/', WeatherMinutelyView.as_view(), name='weather-minutely'),
     path('users/<int:pk>/hourly/', WeatherHourlyView.as_view(), name='weather-hourly'),
     path('users/<int:pk>/daily/', WeatherDailyView.as_view(), name='weather-daily'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('hello/', HelloView.as_view(), name='hello'),
 ]
